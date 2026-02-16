99ac23baf1ffe2861bdb0f150e4bc74cbc28c10e
bbeck2417 <billys.beck@gmail.com>
2026-02-16 10:47:10 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { title } = require("node:process");
+const cors = require("cors");
+const router = express.Router();
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+router.get("/songs", (req, res) => {
+  const songs = [
+    {
+      title: "Uptown Funk",
+      artist: "Bruno Mars",
+      popularity: 10,
+      genre: ["funk", "boogie"],
+      releaseDate: new Date(2011, 10, 11),
+    },
+    {
+      title: "We Found Love",
+      artist: "Rihanna",
+      popularity: 10,
+      genre: ["r & b", "pop"],
+      releaseDate: new Date(2014, 10, 31),
+    },
+  ];
+
+  res.json(songs);
+});
+
+app.use("/api", router);
